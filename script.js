dc8810416c60f882a3205f1d7fc192cf5ab39eeb
josephine olesen <jose496m@stud.kea.dk>
2023-01-05 13:48:53 +0100

    lavet animation samt påbegyndt indhold

---
+++ script.js
@@ -0,0 +1,25 @@
+const vidContainer = document.querySelector("video-player");
+const vid = document.querySelector(".video-player video");
+const btn = document.querySelector(".video-player-button");
+
+function togglePlay() {
+    if (vid.paused) {
+        vid.play()
+        btn.textContent = "Pause"
+        btn.classList.add("playing")
+    } else {
+        vid.pause()
+        btn.textContent = "Afspil"
+        btn.classList.remove("playing")
+    }
+
+}
+
+function videoEnded() {
+    btn.textContent = "Afspil"
+
+    
+}
+
+btn.addEventListener("click", togglePlay);
+vid.addEventListener("ended", videoEnded);
